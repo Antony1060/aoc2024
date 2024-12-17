c765f74d68ed9f9001e34df034f2dd6e420e66aa
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2024-12-17 12:38:11 +0100

    Day seventeen, part one

--- examples/17_1.rs
+++ examples/17_1.rs
@@ -26,7 +26,7 @@ fn parse_register(iter: &mut core::str::Split<&str>) -> u64 {
 fn parse_combo(reg: &mut [u64; 3], op: u8) -> u64 {
     match op {
         0..=3 => op as u64,
-        4..=6 => reg[(op % 3) as usize],
+        4..=6 => reg[((op - 1) % 3) as usize],
         _ => unreachable!(),
     }
 }
@@ -39,26 +39,22 @@ fn do_div(reg: &mut [u64; 3], op: u8) -> u64 {
 }
 
 fn process_adv(reg: &mut [u64; 3], ip: u64, op: u8) -> u64 {
-    println!("doing adv");
     reg[REG_A] = do_div(reg, op);
     ip + 2
 }
 
 fn process_bxl(reg: &mut [u64; 3], ip: u64, op: u8) -> u64 {
-    println!("doing bxl");
     reg[REG_B] ^= op as u64;
     ip + 2
 }
 
 fn process_bst(reg: &mut [u64; 3], ip: u64, op: u8) -> u64 {
-    println!("doing bst");
     let combo = parse_combo(reg, op);
     reg[REG_B] = combo % 8;
     ip + 2
 }
 
 fn process_jnz(reg: &mut [u64; 3], ip: u64, op: u8) -> u64 {
-    println!("doing jnz");
     if reg[REG_A] == 0 {
         return ip + 2;
     }
@@ -66,27 +62,23 @@ fn process_jnz(reg: &mut [u64; 3], ip: u64, op: u8) -> u64 {
     op as u64
 }
 
-fn process_bxc(reg: &mut [u64; 3], ip: u64, op: u8) -> u64 {
-    println!("doing bxc");
+fn process_bxc(reg: &mut [u64; 3], ip: u64, _op: u8) -> u64 {
     reg[REG_B] ^= reg[REG_C];
     ip + 2
 }
 
 fn process_out(reg: &mut [u64; 3], ip: u64, op: u8, stdout: &mut Vec<u8>) -> u64 {
-    println!("doing out");
     let combo = parse_combo(reg, op);
     stdout.push((combo % 8) as u8);
     ip + 2
 }
 
 fn process_bdv(reg: &mut [u64; 3], ip: u64, op: u8) -> u64 {
-    println!("doing bdv");
     reg[REG_B] = do_div(reg, op);
     ip + 2
 }
 
 fn process_cdv(reg: &mut [u64; 3], ip: u64, op: u8) -> u64 {
-    println!("doing cdv");
     reg[REG_C] = do_div(reg, op);
     ip + 2
 }
@@ -119,9 +111,6 @@ fn main() {
     let mut ip: u64 = 0;
     while (ip as usize) < program.len() - 1 {
         let op = program[ip as usize + 1];
-
-        dbg!(program[ip as usize], op);
-        dbg!(&reg);
         match program[ip as usize] {
             INST_ADV => ip = process_adv(&mut reg, ip, op),
             INST_BXL => ip = process_bxl(&mut reg, ip, op),
@@ -133,7 +122,6 @@ fn main() {
             INST_CDV => ip = process_cdv(&mut reg, ip, op),
             _ => unreachable!(),
         }
-        dbg!(&reg);
     }
 
     println!(
