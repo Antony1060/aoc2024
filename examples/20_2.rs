f14265e30cfd6a554203491a5c4e1bf56287c455
Antony1060 <antoniostignjedec@gmail.com>
2024-12-20 18:25:57 +0100

    Day twenty, part two

--- examples/20_2.rs
+++ examples/20_2.rs
@@ -1,33 +1,44 @@
-use std::collections::{HashMap, HashSet, LinkedList};
+use rayon::iter::IndexedParallelIterator;
+use rayon::iter::IntoParallelRefIterator;
+use rayon::iter::ParallelIterator;
+use std::collections::{HashMap, LinkedList};
+use std::io::stdin;
 
 const INPUT: &str = include_str!("../input.txt");
 
-fn neighbours(grid: &[char], curr: usize, m_width: usize, ignore_walls: bool) -> Vec<usize> {
+const CHEATS_ALLOWED: usize = 20;
+
+fn neighbours(grid: &[char], curr: usize, m_width: usize) -> Vec<usize> {
     let mut result = Vec::new();
 
     let x = curr % m_width;
     let y = curr / m_width;
 
-    if x > 0 && (ignore_walls || grid[curr - 1] != '#') {
+    if x > 0 && grid[curr - 1] != '#' {
         result.push(curr - 1);
     }
 
-    if x < m_width - 1 && (ignore_walls || grid[curr + 1] != '#') {
+    if x < m_width - 1 && grid[curr + 1] != '#' {
         result.push(curr + 1);
     }
 
-    if y > 0 && (ignore_walls || grid[curr - m_width] != '#') {
+    if y > 0 && grid[curr - m_width] != '#' {
         result.push(curr - m_width);
     }
 
-    if y < (grid.len() / m_width) - 1 && (ignore_walls || grid[curr + m_width] != '#') {
+    if y < (grid.len() / m_width) - 1 && grid[curr + m_width] != '#' {
         result.push(curr + m_width);
     }
 
     result
 }
 
-fn bfs(grid: &[char], start: usize, end: usize, m_width: usize) -> Option<(usize, Vec<usize>)> {
+fn bfs(
+    grid: &[char],
+    start: usize,
+    end: usize,
+    m_width: usize,
+) -> Option<(Vec<usize>, Vec<usize>)> {
     let mut visited = vec![false; grid.len()];
     let mut distances = vec![usize::MAX; grid.len()];
     let mut path = vec![usize::MAX; grid.len()];
@@ -42,10 +53,10 @@ fn bfs(grid: &[char], start: usize, end: usize, m_width: usize) -> Option<(usize
         visited[node] = true;
 
         if node == end {
-            return Some((distances[node] + 1, path));
+            return Some((distances, path));
         }
 
-        for neighbour in neighbours(grid, node, m_width, false) {
+        for neighbour in neighbours(grid, node, m_width) {
             if visited[neighbour] {
                 continue;
             }
@@ -57,50 +68,6 @@ fn bfs(grid: &[char], start: usize, end: usize, m_width: usize) -> Option<(usize
     }
 
     None
-}
-
-fn bfs_all_paths(
-    grid: &[char],
-    start: usize,
-    end: usize,
-    distance_upper_bound: usize,
-    m_width: usize,
-) -> Vec<usize> {
-    let mut paths: LinkedList<(HashSet<usize>, usize, usize, usize)> = LinkedList::new();
-    paths.push_back((HashSet::from([start]), start, 0, 0));
-
-    let mut result = Vec::new();
-
-    while let Some((visited, last, distance, cheats_used)) = paths.pop_front() {
-        println!("Distance: {}, Cheats used: {}", distance, cheats_used);
-        if distance >= distance_upper_bound || cheats_used > 2 {
-            continue;
-        }
-
-        for neighbour in neighbours(grid, last, m_width, true) {
-            if visited.contains(&neighbour) {
-                continue;
-            }
-
-            if neighbour == end {
-                result.push(distance + 1);
-                continue;
-            }
-
-            let mut new_visited = visited.clone();
-            new_visited.insert(neighbour);
-
-            let new_cheats_used = if grid[neighbour] == '#' || grid[last] == '#' {
-                cheats_used + 1
-            } else {
-                cheats_used
-            };
-
-            paths.push_back((new_visited, neighbour, distance + 1, new_cheats_used));
-        }
-    }
-
-    result
 }
 
 fn main() {
@@ -117,85 +84,105 @@ fn main() {
 
     let end = grid.iter().position(|it| *it == 'E').unwrap();
 
-    let (distance, path) = bfs(&grid, start, end, m_width).unwrap();
+    let (distances, path) = bfs(&grid, start, end, m_width).unwrap();
+
+    let distance = distances[end];
 
     println!("Distance: {}", distance);
 
     let mut full_path: Vec<usize> = Vec::new();
+    full_path.push(end);
     let mut curr = path[end];
+    let mut grid_draw = grid.clone();
     while curr != usize::MAX {
         full_path.push(curr);
         curr = path[curr];
     }
 
-    let all_paths = bfs_all_paths(&grid, start, end, distance - 100, m_width);
+    full_path.reverse();
+
+    for &idx in &full_path {
+        grid_draw[idx] = 'O';
+    }
+
+    let saves: Vec<usize> = full_path
+        .par_iter()
+        .enumerate()
+        .flat_map(|(idx, i)| {
+            let (i_x, i_y) = (i / m_width, i % m_width);
+
+            if idx % 100 == 0 {
+                println!("{}/{}", idx + 1, full_path.len());
+            }
+
+            let mut res = Vec::new();
+
+            for j_idx in (idx + 1)..full_path.len() {
+                let mut grid_cleaned = grid.clone();
+
+                let j = full_path[j_idx];
+                let (j_x, j_y) = (j / m_width, j % m_width);
+
+                let (l_x, l_y) = (i_x.min(j_x), i_y.min(j_y));
+                let (h_x, h_y) = (i_x.max(j_x), i_y.max(j_y));
+                let manhattan = (h_x - l_x) + (h_y - l_y);
+
+                if manhattan > CHEATS_ALLOWED {
+                    continue;
+                }
+
+                for iter_i in l_x..=h_x {
+                    grid_cleaned[iter_i * m_width + (if i_y < j_y { l_y } else { h_y })] = '.';
+                }
+
+                for iter_j in l_y..=h_y {
+                    grid_cleaned[(if i_x < j_x { h_x } else { l_x }) * m_width + iter_j] = '.';
+                }
+
+                let a = i_x * m_width + i_y;
+                let b = j_x * m_width + j_y;
+
+                let dist = distances[a] + manhattan + (distances[end] - distances[b]);
+
+                if dist > distance {
+                    continue;
+                }
+
+                let saved = distance - dist;
+
+                if saved == 0 {
+                    continue;
+                }
+
+                // grid_cleaned[i_x * m_width + i_y] = 's';
+                // grid_cleaned[j_x * m_width + j_y] = 'e';
+
+                // for (idx, val) in grid_cleaned.iter().enumerate() {
+                //     print!("{val}");
+                //     if (idx + 1) % m_width == 0 {
+                //         println!()
+                //     }
+                // }
+
+                // stdin().read_line(&mut String::new());
+
+                res.push(saved);
+            }
+
+            res
+        })
+        .collect();
 
     let mut histogram = HashMap::new();
-    for path_distance in all_paths {
-        let saved = distance - path_distance;
-        let Some(val) = histogram.get_mut(&saved) else {
-            histogram.insert(saved, 1);
+    for save in saves {
+        let Some(val) = histogram.get_mut(&save) else {
+            histogram.insert(save, 1);
             continue;
         };
 
         *val += 1;
     }
 
-    dbg!(&histogram);
-
-    // let cheats = full_path
-    //     .iter()
-    //     .flat_map(|it| {
-    //         let x = it % m_width;
-    //         let y = it / m_width;
-    //
-    //         let mut result = Vec::new();
-    //
-    //         if x > 0 && grid[it - 1] == '#' {
-    //             result.push(it - 1);
-    //         }
-    //
-    //         if x < m_width - 1 && grid[it + 1] == '#' {
-    //             result.push(it + 1);
-    //         }
-    //
-    //         if y > 0 && grid[it - m_width] == '#' {
-    //             result.push(it - m_width);
-    //         }
-    //
-    //         if y < (grid.len() / m_width) - 1 && grid[it + m_width] == '#' {
-    //             result.push(it + m_width);
-    //         }
-    //
-    //         result
-    //     })
-    //     .collect::<HashSet<_>>();
-    //
-    // let mut histogram = HashMap::new();
-    //
-    // for cheat in cheats {
-    //     let mut grid_iter = grid.clone();
-    //     grid_iter[cheat] = '.';
-    //
-    //     let Some((cheat_distance, _)) = bfs(&grid_iter, start, end, m_width) else {
-    //         continue;
-    //     };
-    //
-    //     let saved = distance - cheat_distance;
-    //     if saved == 0 {
-    //         continue;
-    //     }
-    //
-    //     let Some(val) = histogram.get_mut(&saved) else {
-    //         histogram.insert(saved, 1);
-    //         continue;
-    //     };
-    //
-    //     *val += 1;
-    // }
-    //
-    // dbg!(&histogram);
-    //
     let result = histogram
         .into_iter()
         .filter(|(key, _)| *key >= 100)
